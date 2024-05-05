934c586c3b61e124f95dc3da5ab9a1d46ceeda76
sjting8 <122575130+sjting8@users.noreply.github.com>
2024-05-05 13:44:41 -0700

    finished expose

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 5 - Starter
+Sean Ting
+Benjamin Nhan
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,35 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectImage = document.getElementById("horn-select");
+  const result = document.getElementsByTagName("img");
+  const selectAudio = document.getElementsByClassName("hidden");
+  const selectVolume = document.getElementById("volume-controls");
+  const playButton = document.getElementsByTagName("button");
+
+  selectImage.addEventListener("change", (event) => {
+    result[0].src = "assets/images/" + event.target.value + ".svg";
+    selectAudio[0].src = "assets/audio/" + event.target.value + ".mp3";  
+  });
+
+  selectVolume.addEventListener("change", (event) => {
+    selectAudio[0].volume = event.target.value/100;
+    if(event.target.value == 0) {
+      result[1].src = "assets/icons/volume-level-0.svg";
+    }
+    else if(event.target.value < 33) {
+      result[1].src = "assets/icons/volume-level-1.svg";
+    }
+    else if(event.target.value < 67) {
+      result[1].src = "assets/icons/volume-level-2.svg";
+    }
+    else {
+      result[1].src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+  playButton[0].onclick = (event) => {
+    selectAudio[0].play();
+  };
 }
+  
